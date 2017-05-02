fcc46d62aeeb1d08192b25313f415b87e590612c
JNim <justinlnim@gmail.com>
2017-05-01 18:28:46 -0700

    Adding encoder code
    Added encoder code

--- arduino_forward/arduino_forward.ino
+++ arduino_forward/arduino_forward.ino
@@ -111,7 +111,6 @@ void loop() {
 
   }
 
-  delay(1000);
 }
 
 void slowStartMotors(int speed) {
---
+++ encoder.ino
@@ -0,0 +1,76 @@
+
+
+#define encoder0PinA  13
+#define encoder0PinB  1
+
+volatile unsigned int encoder0Pos = 0;
+
+void setup() { 
+
+
+  pinMode(encoder0PinA, INPUT); 
+  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT); 
+  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+
+  attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+  Serial.begin (9600);
+  Serial.println("start");                // a personal quirk
+
+} 
+
+void loop(){
+// do some stuff here - the joy of interrupts is that they take care of themselves
+}
+
+void doEncoder() {
+  /* If pinA and pinB are both high or both low, it is spinning
+   * forward. If they're different, it's going backward.
+   *
+   * For more information on speeding up this process, see
+   * [Reference/PortManipulation], specifically the PIND register.
+   */
+  if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
+    encoder0Pos++;
+  } else {
+    encoder0Pos--;
+  }
+
+  Serial.println (encoder0Pos, DEC);
+}
+
+/* See this expanded function to get a better understanding of the
+ * meanings of the four possible (pinA, pinB) value pairs:
+ */
+void doEncoder_Expanded(){
+  if (digitalRead(encoder0PinA) == HIGH) {   // found a low-to-high on channel A
+    if (digitalRead(encoder0PinB) == LOW) {  // check channel B to see which way
+                                             // encoder is turning
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    } 
+    else {
+      encoder0Pos = encoder0Pos + 1;         // CW
+    }
+  }
+  else                                        // found a high-to-low on channel A
+  { 
+    if (digitalRead(encoder0PinB) == LOW) {   // check channel B to see which way
+                                              // encoder is turning  
+      encoder0Pos = encoder0Pos + 1;          // CW
+    } 
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+
+  }
+  Serial.println (encoder0Pos, DEC);          // debug - remember to comment out
+                                              // before final program run
+  // you don't want serial slowing down your program if not needed
+}
+
+/*  to read the other two transitions - just use another attachInterrupt()
+in the setup and duplicate the doEncoder function into say, 
+doEncoderA and doEncoderB. 
+You also need to move the other encoder wire over to pin 3 (interrupt 1). 
+*/
+
